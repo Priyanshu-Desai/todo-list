966ca59e3cda44ae6ab3ffee9acf0741b002cf7d
Priyanshu-Desai <desai09p@gmail.com>
2021-05-31 19:45:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo-list
+todo list is a react web app for creating a list
---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function App() {
+  const [todoText, setTodoText] = React.useState();
+  const [todoItems, setTodoItems] = React.useState([]);
+
+  function readTodoText(event) {
+    const newValue = event.target.value;
+    setTodoText(newValue);
+  }
+
+  function addTodoItem() {
+    setTodoItems((prevTodoItems) => {
+      return [...prevTodoItems, todoText];
+    });
+    setTodoText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" value={todoText} onChange={readTodoText} />
+        <button onClick={addTodoItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {todoItems.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
