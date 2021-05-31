92ab6c53b6eb79fae2e84c5e986ad8237257a8fe
Priyanshu-Desai <desai09p@gmail.com>
2021-05-31 20:41:12 +0100

    added 'completed' functionality using strikethrough

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 function TodoItem(props) {
-  return <li>{props.text}</li>
+  const [isDone, setIsDone] = React.useState(false)
+
+  function handleClick(){
+    setIsDone(prevValue => {return !prevValue})
+  }
+
+  return <div onClick={handleClick}>
+  <li style={{textDecoration: isDone ? 'line-through' : 'none'}}>{props.text}</li>
+  </div>
 }
 export default TodoItem
